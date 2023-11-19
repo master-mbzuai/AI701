6ffc614a00c43955487d4028cff7bb7a91bcf635
Sebastian Cavada <sebastian.cavada.lab@gmail.com>
2023-11-19 21:52:55 +0400

    cambiare dataset

--- code/experiments/hierarchy.sh
+++ code/experiments/hierarchy.sh
@@ -1,2 +1,4 @@
 cd ..
 xterm -e "python main_hierarchy.py --model_name hierarchy10 --experiment_name hierarchy_10_06 --epochs 50 --lr 0.0001; exit"
+
+python main_hierarchy20.py --model_name hierarchy20 --experiment_name hierarchy_20_new --epochs 50 --lr 0.0001
