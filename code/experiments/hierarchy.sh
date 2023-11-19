359a6a17b28d491eb0c29f694074e516ee99e96a
Sebastian Cavada <sebastian.cavada.lab@gmail.com>
2023-11-19 09:46:35 +0400

    new mapping and cleaning

--- code/experiments/hierarchy.sh
+++ code/experiments/hierarchy.sh
@@ -1,2 +1,2 @@
 cd ..
-xterm -e "python main_hierarchy.py --model_name hierarchy10 --experiment_name hierarchy_10_relu --epochs 50 --lr 0.0001; exit"
+xterm -e "python main_hierarchy.py --model_name hierarchy10 --experiment_name hierarchy_10_new --epochs 50 --lr 0.0001; exit"
