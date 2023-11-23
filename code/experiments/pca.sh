a2c4f22c7afdbba4cf412b0a8e744aef0355a1d2
sebastian <sebastian.cavada.lab@gmail.com>
2023-11-23 17:12:03 +0400

    new things

--- code/experiments/pca.sh
+++ code/experiments/pca.sh
@@ -4,8 +4,8 @@ geometry_settings=("80x24+0+0" "80x24+400+0" "80x24+0+400" "80x24+400+400" "80x2
 
 cd ..
 for i in "${!args[@]}"; do
-  geometry=${geometry_settings[$i % ${#geometry_settings[@]}]}
-  xterm -geometry $geometry -e "python main.py --d ${args[$i]} --model_name original_pre_pca --epochs 100 --experiment_name pca --lr 0.0001; exit" &  
+  geometry=${geometry_settings[$i % ${#geometry_settings[@]}]}  
+  xterm -geometry $geometry -e "python main_hierarchy10.py --d ${args[$i]} --model_name hierarchy10pca --epochs 100 --experiment_name pca_hierarchical --lr 0.0001; exit" &  
 done
 
 xterm -e "python main.py --d 0 --model_name original_pre_pca --epochs 100 --experiment_name pca --lr 0.0001; exit" &  
